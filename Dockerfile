9614467534568b28d724f2a36548d3b2700766a4
Giovanni <cuzztomgdev@gmail.com>
2025-07-08 22:56:38 -0600

    Django welcome message

--- Dockerfile
+++ Dockerfile
@@ -11,7 +11,7 @@ RUN apt-get update && apt-get install -y build-essential libpq-dev
 WORKDIR /code
 
 COPY requirements.txt .
-RUN pip install --no-chache-dir -r requirements.txt
+RUN pip install -r requirements.txt --no-cache-dir
 
 COPY . .
 
--- app/taskfy/urls.py
+++ app/taskfy/urls.py
@@ -1,8 +1,8 @@
-from django.contrib import admin
+# from django.contrib import admin
 from django.urls import path
 from core import views
 
 urlpatterns = [
-    path('adming/', admin.site.urls),
+    # path('admin/', admin.site.urls),
     path('', views.home, name='home'),
 ]
