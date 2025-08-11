034cd216d5962734d70d28631167fd120cf115f5
Gio Arellano <cuzztomgdev@gmail.com>
2025-08-11 13:56:15 -0600

    Fix server listening to vite

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,10 @@ import svgr from "vite-plugin-svgr";
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    host: '0.0.0.0',
+    port: 5173
+  },
   plugins: [
     react(),
     svgr({
