a124ceb74cf7d98e7fc9b76408c8f40741c0d1e1
Gio Arellano <cuzztomgdev@gmail.com>
2025-08-12 12:29:28 -0600

    Change APIView to ListCreateAPIView to get all workspaces memeberships too

--- backend/apps/workspaces/infrastructure/views.py
+++ backend/apps/workspaces/infrastructure/views.py
@@ -1,25 +1,24 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
-from .models import Workspace
-from .serializers import WorkspaceCreateSerializer
+from rest_framework.generics import ListCreateAPIView
 
-class WorkspaceCreateView(APIView):
+from .serializers import WorkspaceSerializer
+from .models import Workspace, WorkspacesMembership
+
+class WorkspaceView(ListCreateAPIView):
+    serializer_class = WorkspaceSerializer
     permission_classes = [IsAuthenticated]
     
-    def post(self, request):
-        serializer = WorkspaceCreateSerializer(data=request.data)
-        if serializer.is_valid():
-            workspace = Workspace.objects.create(name=request.data['name'],
-                                                 description=request.data.get('description', ''),
-                                                 created_by=request.user)
+    
+    def get_queryset(self):
+        """ Return list of workspaces that user authenticated is member """
+        return Workspace.objects.filter(membership__user=self.request.user)
+    
+    
+    def perform_create(self, serializer):
+        workspace = serializer.save(created_by=self.request.user)
+        # Add member relation to creator
+        WorkspacesMembership.objects.create(workspace=workspace,
+                                            user=self.request.user,
+                                            role='owner')
 
-            return Response({'message': f'Workspace {workspace.name} created', 
-                             'created_at': workspace.created_at,
-                             'id': workspace.id})            
-        
-        return Response({'message': 'Invalid data'}, 
-                        status=status.HTTP_400_BAD_REQUEST)
-        
         
