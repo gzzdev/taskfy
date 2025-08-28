e3277f91b5efe1bf84960ff779835fafc2e606b9
Gio Arellano <cuzztomgdev@gmail.com>
2025-08-28 13:21:42 -0600

    feature: members project validation

--- backend/apps/projects/infrastructure/views/member_views.py
+++ backend/apps/projects/infrastructure/views/member_views.py
@@ -1,13 +1,17 @@
 from rest_framework import generics, status
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.exceptions import PermissionDenied
 
 from django.shortcuts import get_object_or_404
 from django.db import IntegrityError
 
+import logging
+logger = logging.getLogger(__name__)
 
 from ..serializers import ProjectMembershipSerializer
-from ..models import ProjectMembership, Project
+from ..models import ProjectMembership
+from apps.workspaces.infrastructure.models import WorkspacesMembership
 
 class MembershipListView(generics.ListCreateAPIView):
     
@@ -16,10 +20,8 @@ class MembershipListView(generics.ListCreateAPIView):
     
     
     def get_queryset(self):
-        project_id, workspace_id = self.kwargs['project_id'], self.kwargs['workspace_id']
-        return ProjectMembership.objects.filter(project_id=project_id, 
-                                                project__workspace_id=workspace_id,
-                                                user=self.request.user)
+        return ProjectMembership.objects.filter(project_id=self.kwargs['project_id'], 
+                                                project__workspace_id=self.kwargs['workspace_id'])
     
     
     def create(self, request, *args, **kwargs):
@@ -29,17 +31,24 @@ class MembershipListView(generics.ListCreateAPIView):
             serializer.is_valid(raise_exception=True)
             self.perform_create(serializer)
         except IntegrityError as e:
-            return Response({"detail": f"User is already a member of this project. {e}"}, status=status.HTTP_409_CONFLICT)
+            return Response({"detail": f"User is already a member of this project."}, status=status.HTTP_409_CONFLICT)
+        
+        except PermissionDenied:
+            return Response({"detail": f"User is not in the project workspace."}, status=status.HTTP_403_FORBIDDEN)
+        
         
         headers = self.get_success_headers(serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
     
     def perform_create(self, serializer):
         project_id, workspace_id = self.kwargs.get('project_id'), self.kwargs.get('workspace_id')
-        project = get_object_or_404(Project, id=project_id, workspace_id=workspace_id)
+        user = serializer.validated_data.get('user')
         
         # Ensure user is part of the workspace
-        # if not project.workspace.membership.filter(user=self.request.user).exists():
-        #     raise PermissionDenied("You are not a member of this workspace.")
+        if not WorkspacesMembership.objects.filter(workspace_id=workspace_id, user=user).exists():
+            logger.warning(f"User {user.email} is not a member of workspace {workspace_id}")
+            raise PermissionDenied()
         
-        serializer.save(project=project)
+        serializer.save(project_id=project_id)
+        
+        
