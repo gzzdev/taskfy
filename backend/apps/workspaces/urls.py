463f8122b7bf0f5daeaacfe4baed59814821f209
Gio Arellano <cuzztomgdev@gmail.com>
2025-09-03 13:20:34 -0600

    fix: workspace endpoint ot get all

--- backend/apps/workspaces/urls.py
+++ backend/apps/workspaces/urls.py
@@ -4,7 +4,7 @@ from rest_framework.routers import DefaultRouter
 from .infrastructure.views import WorkspaceView, MembershipView, DeleteMembershipView
 
 urlpatterns = [
-    path('<int:workspace_id>', WorkspaceView.as_view(), name='workspace'),
+    path('', WorkspaceView.as_view(), name='workspace'),
     path('<int:workspace_id>/members/<int:user_id>', DeleteMembershipView.as_view(), name='workspace-membership-delete'),
     path('<int:workspace_id>/members', MembershipView.as_view(), name='workspace-membership'),
 ]
